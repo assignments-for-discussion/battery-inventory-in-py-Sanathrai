f7f32e9924807e93a62e1ebd12e0216fc4c0b028
Sanathrai <120644453+Sanathrai@users.noreply.github.com>
2022-12-15 16:50:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def count_batteries_by_usage(cycles):
 def test_bucketing_by_number_of_cycles():
   print("Counting batteries by usage cycles...\n");
   counts = count_batteries_by_usage([100, 300, 500, 600, 900, 1000])
-  string count=counts
+  count=string(counts)
   if count>930:
     print("High")
     assert(counts["highCount"] == 1)
